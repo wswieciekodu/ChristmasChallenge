6bdebc25db838ba7140ea32d4c2cae7cb8c985e4
Magda <wswieciekodu@gmail.com>
2022-12-01 00:23:52 +0100

    #1 ChristmasChallenge - Day 1 - Add Day1 exercise

---
+++ src/Day1_Cities.java
@@ -0,0 +1,21 @@
+public class Day1_Cities {
+
+    /**
+     * Wyzwanie Świąteczne @wswieciekodu
+     * Dzień 1
+     * Mikołaj musi odwiedzić 3 miasta o współrzędnych (1,1), (4,5), (11,5).
+     * Na 10 jednostek odległości jego sanie zużywają 20 litrów magicznego paliwa.
+     * Ile paliwa potrzebuje na całą trasę? Do policzenia przygotuj odpowiedni kod.
+     * Obliczony wynik wypisz na ekranie.
+     *
+     * Oblicz w kodzie dystans miedzy miastami, a nastepnie policz niezbedną ilosc paliwa.
+     * Do obliczenia dystansu przygotuj odpowiednią funkcję.
+     * Do obliczeń matematycznych oblicz funkcji z biblioteki Twojego jezyka.
+     */
+
+    public static void main(String[] args) {
+
+        double neededPetrol = 0.0; //TODO: calculate
+
+    }
+}
