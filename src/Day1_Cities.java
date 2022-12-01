7141af162b6f6de003edc4cb7bc07f2ee9b039d4
Magda <wswieciekodu@gmail.com>
2022-12-01 09:27:05 +0100

    #1 ChristmasChallenge - Day 1 - Adjust content of Day1 exercise

--- src/Day1_Cities.java
+++ src/Day1_Cities.java
@@ -4,6 +4,7 @@ public class Day1_Cities {
      * Wyzwanie Świąteczne @wswieciekodu
      * Dzień 1
      * Mikołaj musi odwiedzić 3 miasta o współrzędnych (1,1), (4,5), (11,5) w podanej kolejności.
+     * Mikołaj startuje z punktu(1,1) spod stacji paliw ;)
      * Na 10 jednostek odległości jego sanie zużywają 20 litrów magicznego paliwa.
      * Ile paliwa potrzebuje na całą trasę? Do policzenia przygotuj odpowiedni kod.
      * Obliczony wynik wypisz na ekranie.
