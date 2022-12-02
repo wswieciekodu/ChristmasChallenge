acb11a699647889b4c5f189f65ec9f5f3dbef411
Magda <wswieciekodu@gmail.com>
2022-12-02 09:04:01 +0100

    #2 ChristmasChallenge - Day 2 - Add Day 2 exercise

---
+++ src/Day2_RandomPresent.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.random.RandomGenerator;
+
+public class Day2_RandomPresent {
+
+    /**
+     * Mikołaj postanowił w tym roku skorzystać z pomocy nowoczesnych technologii.
+     * Zamiast zastanawiać się, komu jaki prezent dostarczyć postanowił skorzystać z programu.
+     * Pomoż Mikołajowi!
+     * Przygotuj kod, który z listy dostępnych prezentów wylosuje 3 prezenty i wypisze na ekranie.
+     * Odpal program kilka razy - sprawdź, czy na pewno losuje za kazdym razem inne prezenty!
+     * Wylosowane prezenty wypisz na ekranie. Zadbaj o to, żeby wylosowane prezenty były unikalne.
+     */
+
+    public static void main(String[] args) {
+
+        List<String> ideas = List.of(
+                "Perfume", "Socks", "Sweater", "Cup",
+                "Hat", "Tea", "Coffee", "Clock", "Bag",
+                "Book", "Wallet", "Cream", "Earrings");
+
+        // TODO: add your code here
+
+    }
+}
