e1fcd7484de576219b8c0386c116f8d44f3a6771
Magda <wswieciekodu@gmail.com>
2022-12-02 17:43:30 +0100

    #2 ChristmasChallenge - Day 2 - Add Day 2 exercise solution

--- src/Day2_RandomPresent.java
+++ src/Day2_RandomPresent.java
@@ -1,5 +1,8 @@
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.random.RandomGenerator;
+import java.util.stream.Collectors;
 
 public class Day2_RandomPresent {
 
@@ -11,6 +14,7 @@ public class Day2_RandomPresent {
      * Odpal program kilka razy - sprawdź, czy na pewno losuje za kazdym razem inne prezenty!
      * Wylosowane prezenty wypisz na ekranie. Zadbaj o to, żeby wylosowane prezenty były unikalne.
      */
+    public static final int GIFTS_SIZE = 3;
 
     public static void main(String[] args) {
 
@@ -19,7 +23,18 @@ public class Day2_RandomPresent {
                 "Hat", "Tea", "Coffee", "Clock", "Bag",
                 "Book", "Wallet", "Cream", "Earrings");
 
-        // TODO: add your code here
+        RandomGenerator randomGenerator = RandomGenerator.getDefault();
 
+        Set<Integer> randomNumbers = new HashSet<>();
+
+        while (randomNumbers.size() != GIFTS_SIZE) {
+            randomNumbers.add(randomGenerator.nextInt(ideas.size()));
+        }
+
+        List<String> randomGifts = randomNumbers.stream()
+                .map(ideas::get)
+                .toList();
+
+        System.out.println("Random gifts: " + randomGifts);
     }
 }
