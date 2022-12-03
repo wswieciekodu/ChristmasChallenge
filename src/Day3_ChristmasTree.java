55eac3028c2d03b44755b729c80585f769d372d9
Magda <wswieciekodu@gmail.com>
2022-12-03 02:10:34 +0100

    #3 ChristmasChallenge - Day 3 - Add Day 3 exercise description

---
+++ src/Day3_ChristmasTree.java
@@ -0,0 +1,19 @@
+public class Day3_ChristmasTree {
+
+    /**
+     * Mikołaj postanowił w tym roku skorzytać z pomocy nowoczesnych technologii.
+     * Zamiast kupować choinkę i ozdoby postanowił użyć takich wygenerowanych w konsoli (terminalu)
+     * Pomoż Mikołajowi! Przygotuj kod, który wypisze na ekranie choinkę.
+     * Jeżeli potrafisz, nie wpisuj drzewka bezposrednio w printy - pokombinuj z pętlami.
+     * Przygotuj funkcję przyjmującą wysokość choinki i wypisującą choinkę na ekranie.
+     * Tak, żeby choinka wygenerowała się sama! Choinka może być po prostu trójkątem, albo być żłożona z kilku warstw. :)
+     */
+
+    public static void main(String[] args) {
+
+        int height = 15;
+
+        // TODO: prepare function which draw christmas tree;
+    }
+
+}
