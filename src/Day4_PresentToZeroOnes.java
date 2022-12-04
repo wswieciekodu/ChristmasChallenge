ace06d8c03f71a1fe4a0251b8feac14d5bd50b9e
Magda <wswieciekodu@gmail.com>
2022-12-04 01:01:15 +0100

    #4 ChristmasChallenge - Day 4 - Add Day 4 exercise

---
+++ src/Day4_PresentToZeroOnes.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class Day4_PresentToZeroOnes {
+
+    /**
+     * Mikołaj postanowił w tym roku skorzytać z pomocy nowoczesnych technologii
+     * i zamiast rozwozić wszystkie prezenty, niektóre przesłać przez Internet.
+     * Do tego celu musi zamienić nazwy prezentów w strumienie bitów!
+     * Pomoż Mikołajowi! Przygotuj kod, który zamieni nazwy prezentów na ciąg zer i jedynek.
+     * Dla każdej litery nazwy prezentu znajdz kod UTF-8, a później zamień go na system binarny.
+     * Dla każdego prezentu wypisz na ekran ciąg zer i jedynek. Każdy prezent w oddzielnej linijce.
+     * Możesz napisać funkcję zamieniającą numer znaku na system binarny samodzielnie,
+     * albo znaleźć odpowiednią funkcję w bibliotece Twojego języka.
+     */
+
+    public static void main(String[] args) {
+
+        List<String> ideas = List.of(
+                "Parfum", "Socks", "Sweather", "Cup",
+                "Hat", "Tea", "Coffee", "Clock", "Bag",
+                "Book", "Wallet", "Cream", "Earrings");
+
+        // TODO: Add your code here
+    }
+
+}
