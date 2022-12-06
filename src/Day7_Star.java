a1c2253f32e5615fc43a07e13b1418733125c624
Magda <wswieciekodu@gmail.com>
2022-12-07 00:37:01 +0100

    #7 ChristmasChallenge - Day 7 - Add Day 7 exercise

---
+++ src/Day7_Star.java
@@ -0,0 +1,23 @@
+public class Day7_Star {
+
+    /**
+     * Mikołaj postanowił w tym roku skorzytać z pomocy nowoczesnych technologii.
+     * Zamiast kupować choinkę i ozdoby postanowił użyć takich wygenerowanych w konsoli
+     * Pomoż Mikołajowi! Przygotuj kod, który wypisze na ekranie gwiazdkę.
+     * Jeżeli potrafisz, nie wpisuj gwiazdki bezposrednio w printy - pokombinuj z pętlami.
+     * Tak, żeby gwiazdka wygenerowała się sama!
+     * Gwiazdka może mieć dowolny kształt, wygeneruj taki jaki potrafisz. Przykładowe gwiazdki:
+     *     *       *       *         *
+     *     *         *   *        *******
+     * *********   **********      *****
+     *     *         *   *        *******
+     *     *       *       *         *
+     */
+
+    public static void main(String[] args) {
+        int size = 15;
+
+        // TODO: draw star in console for given size
+
+    }
+}
