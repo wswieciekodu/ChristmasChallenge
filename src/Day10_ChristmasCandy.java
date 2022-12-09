849d314044c1202f87a5a77148bffddf0f0c6973
Magda <wswieciekodu@gmail.com>
2022-12-10 00:02:51 +0100

    #10 ChristmasChallenge - Day 10 - Add Day 10 exercise

---
+++ src/Day10_ChristmasCandy.java
@@ -0,0 +1,17 @@
+public class Day10_ChristmasCandy {
+
+    /**
+     * Mikołaj postanowił w tym roku skorzytać z pomocy nowoczesnych technologii.
+     * Zamiast kupować choinkę i ozdoby postanowił użyć takich wygenerowanych w konsoli (terminalu)
+     * Pomoż Mikołajowi! Przygotuj kod, który wypisze na ekranie laskę cukrową.
+     * Jeżeli potrafisz, nie wpisuj laski bezpośrednio w printy - pokombinuj z pętlami.
+     * Tak, żeby laska wygenerowała się sama!
+     */
+
+    public static void main(String[] args) {
+
+        int height = 13;
+
+        // TODO: prepare function which draw in console christmas candy
+    }
+}
