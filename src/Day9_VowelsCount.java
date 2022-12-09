2f588d96ee553f13b13461057abfcd516720f91c
Magda <wswieciekodu@gmail.com>
2022-12-09 21:29:25 +0100

    #9 ChristmasChallenge - Day 9 - Add Day 9 exercise solutions

--- src/Day9_VowelsCount.java
+++ src/Day9_VowelsCount.java
@@ -2,6 +2,8 @@ import model.Address;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day9_VowelsCount {
 
@@ -14,9 +16,49 @@ public class Day9_VowelsCount {
 
     public static void main(String[] args) {
         List<Address> addresses = prepareAddresses();
+        int vowelsCount = 0;
 
-        //TODO: Calculate how much Santa need to additionally pay
+        for (Address address : addresses) {
+            vowelsCount += countVowelsStepByStep(address.getCity());
+            vowelsCount += countVowelsStepByStep(address.getStreet());
+            vowelsCount += countVowelsStepByStep(address.getNumber());
+        }
 
+        // FIXME: You can also use shorter version with IntStream or Regex. Check methods below.
+        // FIXME: You can also use shorter version with regex. Check getVowelsShortVersion method below.
+
+        int pricePerVowel = 2;
+        System.out.println("Santa needs to pay: " + vowelsCount * pricePerVowel + "zl");
+    }
+
+    private static int countVowelsStepByStep(String string) {
+
+        int vowelCount = 0;
+        List<Character> vowels = List.of('a', 'e', 'y', 'i', 'o', 'u');
+        String stringLowercase = string.toLowerCase();
+
+        for (int i = 0; i < string.length(); i++) {
+            if (vowels.contains(stringLowercase.charAt(i))) {
+                vowelCount++;
+            }
+        }
+
+        return vowelCount;
+    }
+
+    private static long countVowelsUsingIntStream(String string) {
+        List<Character> vowels = List.of('a', 'e', 'y', 'i', 'o', 'u');
+
+        return string.toLowerCase()
+                .chars()
+                .filter(ch -> vowels.contains((char) ch))
+                .count();
+    }
+
+    public static long countVowelsUsingRegex(String string) {
+        Pattern pattern = Pattern.compile("[aeiouy]");
+        Matcher matcher = pattern.matcher(string.toLowerCase());
+        return matcher.results().count();
     }
 
     private static List<Address> prepareAddresses() {
