ded58b160189bdc8c91b71832b70f39827b2102a
Magda <wswieciekodu@gmail.com>
2022-12-13 00:18:44 +0100

    #13 ChristmasChallenge - Day 13 - Add Day 13 exercise solution

---
+++ src/Day13_RevertNames.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day13_RevertNames {
+
+    /**
+     * Chochliki nie ułatwiają życia Mikołajowi. Tym razem poodwracały imiona dzieci na liście…
+     * Napraw co popsuły chochliki i odwróć imiona.
+     * Zwróć uwagę na duże litery i zamień je, tak, żeby imiona były poprawne.
+     * Na koniec wypisz listę w terminalu żeby sprawdzić, czy wszystko jest ok!
+     */
+
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Uisaj");
+        names.add("Aisak");
+        names.add("Aisa");
+        names.add("Kemot");
+        names.add("Lahcim");
+
+        // TODO: correct names from list, with modifying or not modifying list
+    }
+}
