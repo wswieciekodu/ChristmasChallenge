62316b11126fe2e47659a8ac9138641b5e4f9427
Magda <wswieciekodu@gmail.com>
2022-12-13 17:41:25 +0100

    #13 ChristmasChallenge - Day 13 - Add Day 13 exercise solution

--- src/Day13_RevertNames.java
+++ src/Day13_RevertNames.java
@@ -18,6 +18,34 @@ public class Day13_RevertNames {
         names.add("Kemot");
         names.add("Lahcim");
 
-        // TODO: correct names from list, with modifying or not modifying list
+        // Simplest, step by step version with loop
+        for (int i = 0; i < names.size(); i++) {
+            String correctName = reverseStringStepByStep(names.get(i));
+            names.set(i, correctName);
+        }
+        System.out.println(names);
+
+        // Shorter version with StringBuilder methods
+        // names.replaceAll(Day13_RevertNames::reverseStringUsingStringBuilder);
+    }
+
+    private static String reverseStringUsingStringBuilder(String string) {
+        String reversedLowercase = new StringBuilder(string).reverse().toString().toLowerCase();
+        return reversedLowercase.substring(0, 1).toUpperCase() + reversedLowercase.substring(1);
+    }
+
+    private static String reverseStringStepByStep(String string) {
+        String stringInLowercase = string.toLowerCase();
+        StringBuilder reversedString = new StringBuilder();
+
+        for (int i = stringInLowercase.length() - 1; i >= 0; i--) {
+            reversedString.append(stringInLowercase.charAt(i));
+        }
+
+        String firstLetterUppercase = string.substring(string.length() - 1).toUpperCase();
+
+        reversedString.replace(0, 1, firstLetterUppercase);
+
+        return reversedString.toString();
     }
 }
