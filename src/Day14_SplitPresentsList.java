a32ba6b7b8632d4ba9acc8a13aac467d80b5a115
Magda <wswieciekodu@gmail.com>
2022-12-14 00:36:13 +0100

    #14 ChristmasChallenge - Day 14 - Add Day 14 exercise

---
+++ src/Day14_SplitPresentsList.java
@@ -0,0 +1,17 @@
+public class Day14_SplitPresentsList {
+
+    /**
+     * Lista prezentów od dzieci przychodzi w jednym długim napisie.
+     * Napis zawiera dane w formacie: imię dziecka, spacja, prezenty rozdzielone spacjami, przecinek.
+     * Pomóż Mikołajowi! Podziel napis na ciągi: imię dziecka => prezenty po przecinku.
+     * Po ostatnim prezencie w linii nie powinno byc przecinka.
+     */
+
+    public static void main(String[] args) {
+
+        String giftsList = "Kuba klocki klawiatura dron quad, Alicja komputer samochod mysz, "
+                + "Janina ksiazka traktor, Tomek kuchenka koparka flamastry, ";
+
+        // TODO: Split string and print children name => gifts in one line
+    }
+}
