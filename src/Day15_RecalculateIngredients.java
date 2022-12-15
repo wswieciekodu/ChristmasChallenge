970971d240dd1a24b62aa0c3da7e4416bc7e643a
Magda <wswieciekodu@gmail.com>
2022-12-15 01:23:27 +0100

    #15 ChristmasChallenge - Day 15 - Improve Day 15 exercise description

--- src/Day15_RecalculateIngredients.java
+++ src/Day15_RecalculateIngredients.java
@@ -9,14 +9,14 @@ public class Day15_RecalculateIngredients {
      * Mama przesłała Mikołajowi przepisy na ciasta. Blacha mamy Mikołaja jest prostokątna i ma wymiary 20cm x 30cm.
      * Mikołaj ma blachę okrągłą o średnicy 9.8 cm.
      * Pomóż Mikołajowi przeliczuć składniki przepisu na jego blaszkę.
-     * Przygotuj funkcję, która będzie przyjmować 2 wymiary blaszki prostokątnej oraz średnicę okrąglęj
+     * Przygotuj funkcję, która będzie przyjmować 2 wymiary blaszki prostokątnej oraz średnicę okrąglej
      * i dla podanej listy składników dla prostokątnej przeliczy ich ilość dla okrągłej.
-     * Dla uproszczenia nie przejmj się jednostkami i załóż, że formy mają taką samą wysokość.
+     * Dla uproszczenia nie przejmuj się jednostkami i załóż, że formy mają taką samą wysokość.
      */
 
     public static void main(String[] args) {
 
-        Map<String, Integer> ingredientsForSquareForm = Map.of(
+        Map<String, Integer> ingredientsForRectangleForm = Map.of(
                 "Maka", 300,
                 "Cukier", 100,
                 "Jajka", 4,
