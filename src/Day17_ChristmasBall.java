ed234b32d49f168ef4e9ae270ec5c05132364de2
Magda <wswieciekodu@gmail.com>
2022-12-16 23:56:23 +0100

    #17 ChristmasChallenge - Day 17 - Add Day 17 exercise

---
+++ src/Day17_ChristmasBall.java
@@ -0,0 +1,18 @@
+public class Day17_ChristmasBall {
+
+    /**
+     * Mikołaj postanowił w tym roku skorzytać z pomocy nowoczesnych technologii.
+     * Zamiast kupować choinkę i ozdoby postanowił użyć takich wygenerowanych w konsoli (terminalu)
+     * Pomoż Mikołajowi! Przygotuj kod, który wypisze na ekranie bombkę choinkową.
+     * Jeżeli potrafisz, nie wpisuj bombki bezpośrednio w printy - pokombinuj z pętlami.
+     * Tak, żeby bombka wygenerowała się sama!
+     * Bombka może mieć dowolny kształt, ale najlepiej, żeby była okrągła. :)
+     */
+
+    public static void main(String[] args) {
+
+        int height = 25;
+
+        // TODO: draw christmas ball in console!
+    }
+}
