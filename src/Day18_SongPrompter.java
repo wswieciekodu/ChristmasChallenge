d70d79af6acf08a2e7fe0884bcc2d5b159248c9c
Magda <wswieciekodu@gmail.com>
2022-12-18 00:19:58 +0100

    #18 ChristmasChallenge - Day 18 - Add Day 18 exercise

---
+++ src/Day18_SongPrompter.java
@@ -0,0 +1,22 @@
+public class Day18_SongPrompter {
+    /**
+     * W świeta lubimy śpiewać koledy i świąteczne piosenki. Przygotuj prompter dla koledy lub piosenki.
+     * Niech tekst linijka po linijce wyświetla sie co 3 sekunde w terminalu.
+     * Przykladowy tekst znajdziesz ponizej, ale mozesz zamienić go na tekst Twojej ulubionej piosenki lub koledy!
+     */
+
+    public static void main(String[] args) {
+
+        String lyrics = "Bog sie rodzi, moc truchleje\n"
+                + "Pan niebiosow obnazony!\n"
+                + "Ogien krzepnie, blask ciemnieje.\n"
+                + "Ma granice Nieskonczony\n"
+                + "Wzgardzony, okryty chwala\n"
+                + "Smiertelny Krol nad wiekami!\n"
+                + "A Slowo Cialem sie stalo\n"
+                + "I mieszkalo miedzy nami";
+
+        // TODO: prepare song prompter
+
+    }
+}
