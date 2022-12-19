19979fb02dceacf6f403ff8e48e1fc82ba339e6e
Magda <wswieciekodu@gmail.com>
2022-12-19 21:12:12 +0100

    #19 ChristmasChallenge - Day 19 - Add Day 19 exercise solution

--- src/Day19_WrappingPaper.java
+++ src/Day19_WrappingPaper.java
@@ -16,9 +16,29 @@ public class Day19_WrappingPaper {
     public static void main(String[] args) {
 
         Box small = new Box(10, 13, 15);
-        Box medium = new Box(20, 15, 10);
-        Box big = new Box(30, 25, 20);
+        int paperForSmallBox = calculateSurfaceOfBox(small.getX(), small.getY(), small.getZ());
 
-        // TODO: calculate needed wrapping paper
+        Box medium = new Box(20, 15, 10);
+        int paperForMediumBox = calculateSurfaceOfBox(medium.getX(), medium.getY(), medium.getZ());
+
+        Box big = new Box(30, 25, 20);
+        int paperForBigBox = calculateSurfaceOfBox(big.getX(), big.getY(), big.getZ());
+
+        int smallBoxesAmount = 5;
+        int mediumBoxesAmount = 10;
+        int bigBoxesAmount = 3;
+
+        int neededPaperInCm2 = paperForSmallBox * smallBoxesAmount + paperForMediumBox * mediumBoxesAmount + paperForBigBox * bigBoxesAmount;
+        int neededPaperInM2 = (int) ceil(convertCm2ToM2(neededPaperInCm2));
+
+        System.out.println("Santa needs " + neededPaperInM2 + " m2 of wrapping paper.");
+    }
+
+    private static double convertCm2ToM2(int cm2) {
+        return cm2 / 10000.0;
+    }
+
+    private static int calculateSurfaceOfBox(int x, int y, int z) {
+        return x * y * 2 + x * z * 2 + y * z * 2;
     }
 }
