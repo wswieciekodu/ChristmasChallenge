bd9c224470925b022af911562c1258c5bff42eae
Magda <wswieciekodu@gmail.com>
2022-12-19 23:54:19 +0100

    #20 ChristmasChallenge - Day 20 - Add Day 20 exercise

---
+++ src/Day20_JingleRhythm.java
@@ -0,0 +1,23 @@
+public class Day20_JingleRhythm {
+
+    /**
+     * Świąteczne piosenki to coś co lubimy wszyscy. Niektórzy lubią ich słuchać, ale niektorzy także grać na pianinie!
+     * Przygotuj symulator rytmu piosenki Jingle Bells. W tablicy poniżej znajdziesz długości kolejnych nut.
+     * Na terminalu wypisuj kolejne gwiazdki dla każdej nuty przez określony długością nuty czas.
+     * Przykładowo 300ms * czas określony dla nuty w tablicy. Dla efektu dźwiękowego możesz też użyć beep!
+     */
+
+    public static void main(String[] args) {
+
+        int[] tones = {
+                2, 2, 4, 2, 2, 4, 2, 2, 2, 1, 8, 2, 2, 2, 1, 2, 2, 2, 1, 1,
+                2, 2, 2, 2, 4, 4, 2, 2, 4, 2, 2, 4, 2, 2, 2, 1, 8, 2, 2, 2, 1,
+                2, 2, 2, 1, 1, 2, 2, 2, 2, 8, 2, 2, 2, 2, 4, 1, 1, 2, 2, 2, 2, 8,
+                2, 2, 2, 2, 8, 2, 2, 2, 2, 8, 2, 2, 2, 2, 8,
+                2, 2, 2, 2, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 8
+        };
+
+        // TODO: Show star for as long as tone in table, to make better effect use "beep" function
+
+    }
+}
