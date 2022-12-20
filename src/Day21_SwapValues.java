f73c69ac5fc0764cc4308396cc115ccddc856994
Magda <wswieciekodu@gmail.com>
2022-12-21 00:50:30 +0100

    #21 ChristmasChallenge - Day 21 - Add Day 21 exercise

---
+++ src/Day21_SwapValues.java
@@ -0,0 +1,22 @@
+public class Day21_SwapValues {
+    /**
+     * Mikołaj znów przyoszczedził na pakiecie. Tym razem dla ilości zmiennych.
+     * Kupił sobie tylko dwie: jedną, żeby zapisać ilość miejsc do odwiedzenia,
+     * a drugą, żeby zapisać ilość reniferów.
+     * Niestety! W ferworze świątecznych przygotowań pomylił się i przypisał wartości na odwrót!
+     * Pomoż Mikołajowi i zamień wartości. Pamiętaj, żeby nie używać żadnych dodatkowych zmiennych!
+     * Grozi Mikołajowi za to duża kara pieniężna i gdyby ją otrzymał, nie miał by pieniędzy na prezenty!
+     */
+    public static void main(String[] args) {
+        int places = 10;
+        int reindeers = 100;
+
+        System.out.println("Before switch: places = " + places);
+        System.out.println("Before switch: reindeers = " + reindeers);
+
+        // TODO: swap values not using any additional variable
+
+        System.out.println("After switch: places = " + places);
+        System.out.println("After switch: reindeers = " + reindeers);
+    }
+}
