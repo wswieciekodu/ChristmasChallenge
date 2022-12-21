ce239b8034d7d20b8adb9fc2231d6b2c1c4dc453
Magda <wswieciekodu@gmail.com>
2022-12-22 00:40:23 +0100

    #22 ChristmasChallenge - Day 22 - Add Day 22 exercise

---
+++ src/Day22_GiftBoxRibbon.java
@@ -0,0 +1,18 @@
+import model.Box;
+
+public class Day22_GiftBoxRibbon {
+    /**
+     * Mikołaj zapakował prezenty w pudełka i papier i chce jeszcze obwiązać je wstążką.
+     * Ma 5 pudełek o wielkości 10cmx13cmx15cm.
+     * Oblicz i wypisz na ekranie, ile metrów wstążki musi kupić Mikołaj.
+     * Załóż, że dla każdego prezentu musi kupić tyle wstążki,
+     * żeby obwiązać prezent oddzielnie z dwóch stron + dla każdej strony mieć zapas 50%.
+     * Niech wstązka krzyzuje sie na sciance o największej powierzchni.
+     */
+    public static void main(String[] args) {
+        Box box = new Box(10, 13, 15);
+        int boxesAmount = 5;
+
+        //TODO: calculate how many meters of ribbon Santa needs to buy
+    }
+}
