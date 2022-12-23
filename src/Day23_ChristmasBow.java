38926b23861793b65c151a87359763fadab616db
Magda <wswieciekodu@gmail.com>
2022-12-24 00:54:01 +0100

    #23 ChristmasChallenge - Day 23 - Add Day 23 exercise solution

--- src/Day23_ChristmasBow.java
+++ src/Day23_ChristmasBow.java
@@ -2,7 +2,7 @@ public class Day23_ChristmasBow {
 
     /**
      * Mikołaj postanowił w tym roku skorzytać z pomocy nowoczesnych technologii.
-     * Zamiast kupować choinkę i ozdoby postanowił użyć takich wygenerowanych w konsoli
+     * Zamiast kupować choinkę i ozdoby postanowił użyć takich wygenerowanych w konsoli.
      * Pomoż Mikołajowi! Przygotuj kod, który wypisze na ekranie kokardkę na choinkę.
      * Jeżeli potrafisz, nie wpisuj kokardki bezpośrednio w printy - pokombinuj z pętlami.
      * Tak, żeby kokardka wygenerowała się sama!
@@ -10,8 +10,27 @@ public class Day23_ChristmasBow {
 
     public static void main(String[] args) {
 
-        int height = 15;
+        int height = 8;
 
-        // TODO: draw bow
+        drawBow(height);
     }
+
+    private static void drawBow(int height) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < height; x++) {
+                if (insideBow(x, y, height)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean insideBow(int x, int y, int height) {
+        return (x >= y && x < height - y) ||
+                (x < y && x >= height - y);
+    }
+
 }
