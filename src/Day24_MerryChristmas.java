db1c5b8a9eb6c1cac8fb77e6f557e79fbf6b5178
Magda <wswieciekodu@gmail.com>
2022-12-24 00:55:35 +0100

    #24 ChristmasChallenge - Day 24 - Add Day 24 exercise solution

---
+++ src/Day24_MerryChristmas.java
@@ -0,0 +1,17 @@
+public class Day24_MerryChristmas {
+
+    /**
+     * Dzisiaj już nić nie koduj!
+     * Spędź czas świętując i odpoczywając.
+     * Po tylu dniach Ci się zdecydowanie należy. :D
+     *
+     * Gratuluję Ci, że dotarłe/aś do tego miejsca!
+     * I dziękuję za udział w wyzwaniu!!!
+     * Do zobaczenia na instagramie
+     * Magda, @wswieciekodu <3
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Wesołych Świąt!");
+    }
+}
